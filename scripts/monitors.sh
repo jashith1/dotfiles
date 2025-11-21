4c6185b55250e3384079c04fba36dce7c3dcb33d
Jashith <jashith.r1@gmail.com>
2025-11-21 12:17:41 -0600

    create monitor management script

---
+++ scripts/monitors.sh
@@ -0,0 +1,14 @@
+# detect laptop display name (eDP-1 or eDP-2)
+LAPTOP=$(hyprctl monitors all | grep -Eo 'eDP-[0-9]')
+
+# check if ANY external monitor is connected
+EXT=$(hyprctl monitors all | grep -E 'DP-[0-9]|HDMI-A-[0-9]')
+
+if [ -n "$EXT" ]; then
+    # external monitor exists so disable laptop screen
+    hyprctl keyword monitor "$LAPTOP, disable"
+else
+    # no external monitors so enable laptop screen
+    hyprctl keyword monitor "$LAPTOP, 1920x1200@120, 0x0, 1"
+fi
+
