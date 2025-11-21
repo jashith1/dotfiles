4fde1e2858e944838bb7a21a57f8449485f6c5a8
Jashith <jashith.r1@gmail.com>
2025-11-21 12:49:59 -0600

    revert to using kanshi

--- scripts/monitors.sh
+++ scripts/monitors.sh
@@ -1,11 +1,14 @@
+# just using kanshi since this is worse but still could be useful in the future
+
 # detect laptop display name (eDP-1 or eDP-2)
 LAPTOP=$(hyprctl monitors all | grep -Eo 'eDP-[0-9]')
 
 # check if ANY external monitor is connected
-EXT=$(hyprctl monitors all | grep -E 'DP-[0-9]|HDMI-A-[0-9]')
+EXT=$(hyprctl monitors all | grep -Eo '(^| )DP-[0-9]|(^| )HDMI-A-[0-9]')
 
 if [ -n "$EXT" ]; then
     # external monitor exists so disable laptop screen
+    hyprctl keyword monitor "$EXT, 3840x2160@240, 0x0, 1.5"
     hyprctl keyword monitor "$LAPTOP, disable"
 else
     # no external monitors so enable laptop screen
