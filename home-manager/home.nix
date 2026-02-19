a7a0c6f5c94ade8d02d58a0fec3eeac66c4aabd6
Jashith <jashith.r1@gmail.com>
2026-02-19 00:09:05 -0600

    move some programs to home manager

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,72 +1,14 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "bloppai";
   home.homeDirectory = "/home/bloppai";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "25.11"; # Please read the comment before changing.
+  home.stateVersion = "25.11"; # like nixos stateVersion, dont change
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
   home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
-
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/bloppai/etc/profile.d/hm-session-vars.sh
-  #
   home.sessionVariables = {
     NIXOS_OZONE_WL = "1"; #wayland apps use ozone
     grim="grimblast";
@@ -76,25 +18,59 @@
   };
 
   # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
-  programs.caelestia = {
-    enable = true;
-    systemd = {
-      #enable = false; # if you prefer starting from your compositor
-      target = "graphical-session.target";
-      environment = [];
-    };
-    settings = {
-      bar.status = {
-        #showBattery = false;
+  
+  programs = {
+    home-manager.enable = true;
+
+    caelestia = {
+      enable = true;
+      systemd = {
+        #enable = false; # if you prefer starting from your compositor
+        target = "graphical-session.target";
+        environment = [];
       };
-      paths.wallpaperDir = "~/.config/wallpapers/astronaut.png";
-    };
-    cli = {
-      enable = true; # Also add caelestia-cli to path
       settings = {
-        theme.enableGtk = false;
+        bar.status = {
+          showBattery = true;
+        };
+        paths.wallpaperDir = "~/.config/wallpapers/astronaut.png";
+      };
+      cli = {
+        enable = true; # Also add caelestia-cli to path
+        settings = {
+          theme.enableGtk = false;
+        };
       };
     };
+
+    bash = {
+      enable = true;
+      initExtra = ''
+        testCpp() {
+          if [ -z "$1" ]; then
+            make -C tests -j12 run-all -k
+          else
+            make -C tests -j12 run/$1
+          fi
+        }
+      '';
+    };
+
+
+    firefox.enable = true;
+    chromium.enable = true;
+
+    neovim = {
+      enable = true;
+      defaultEditor = true;
+      viAlias = true;
+      vimAlias = true;
+    };
+
+    git.enable = true;
+    tmux.enable = true;
+
+    waybar.enable = true;
+    hyprlock.enable = true;
   };
 }
