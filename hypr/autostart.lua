a241f8941f4004d33236e45e7004ecb69d3531d6
Jashith Raghavendra <jashith.r1@gmail.com>
2026-08-17 01:03:32 -0500

    add empty luarc for marker and commit nvim lock

--- hypr/autostart.lua
+++ hypr/autostart.lua
@@ -1,7 +1,7 @@
 hl.on("hyprland.start", function()
-    hl.exec_cmd("[workspace 1] " .. Terminal)
+    hl.exec_cmd("[workspace 1 silent] " .. Terminal)
     hl.exec_cmd("[workspace 2 silent] " .. Browser)
-    hl.exec_cmd("[workspace 4 silent] " .. MusicPlayer)
+    hl.exec_cmd("[workspace 4 silent] sleep 1 && " .. MusicPlayer)
 
     -- TODO: find out why this is using sleep lol
     hl.exec_cmd("sleep 1 && caelestia-shell")
