b06d286972dc872b830f59425ecf10793b41c347
Jashith Raghavendra <jashith.r1@gmail.com>
2026-09-20 17:14:41 -0500

    auto format on save and add ruff and rustfmt as formatters

--- nvim/init.lua
+++ nvim/init.lua
@@ -770,24 +770,19 @@ do
   vim.pack.add { gh 'stevearc/conform.nvim' }
   require('conform').setup {
     notify_on_error = false,
-    format_on_save = function(bufnr)
-      -- You can specify filetypes to autoformat on save here:
-      local enabled_filetypes = {
-        -- lua = true,
-        -- python = true,
-      }
-      if enabled_filetypes[vim.bo[bufnr].filetype] then
-        return { timeout_ms = 500 }
-      else
-        return nil
-      end
-    end,
+
+    format_on_save = {
+      timeout_ms = 500,
+      lsp_format = 'fallback',
+    },
+
     default_format_opts = {
       lsp_format = 'fallback', -- Use external formatters if configured below, otherwise use LSP formatting. Set to `false` to disable LSP formatting entirely.
     },
     -- You can also specify external formatters in here.
     formatters_by_ft = {
-      -- rust = { 'rustfmt' },
+      rust = { 'rustfmt' },
+      python = { "ruff_format" },
       -- Conform can also run multiple formatters sequentially
       -- python = { "isort", "black" },
       --
